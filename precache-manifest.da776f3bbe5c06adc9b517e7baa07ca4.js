c0fd44c2693f665ea5c40243727c494b10f1eb46
arielBravo123 <ariel.bravo@ucuenca.edu.ec>
2020-06-14 19:57:05 -0500

    Updates

---
+++ precache-manifest.da776f3bbe5c06adc9b517e7baa07ca4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34628c6878aaf4c4db8ced5995991009",
+    "url": "/monsters-react/index.html"
+  },
+  {
+    "revision": "ab69b404d7537904750e",
+    "url": "/monsters-react/static/css/main.4634ba57.chunk.css"
+  },
+  {
+    "revision": "80de6e261c3a1f36230c",
+    "url": "/monsters-react/static/js/2.05463ef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-react/static/js/2.05463ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab69b404d7537904750e",
+    "url": "/monsters-react/static/js/main.c6016473.chunk.js"
+  },
+  {
+    "revision": "d4a095b8067e5f24bf5b",
+    "url": "/monsters-react/static/js/runtime-main.d5093ca6.js"
+  }
+]);
